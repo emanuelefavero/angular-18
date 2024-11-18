f39a397e89b9851b5f35fd2a17e52f803bd4b7c7
emanuelefavero <emafaux89@gmail.com>
2024-11-18 07:41:44 +0100

    add template-form component

---
+++ src/app/components/template-form/template-form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-form',
+  standalone: true,
+  imports: [FormsModule],
+  template: `
+    <h2 class="mt-4">Template Form:</h2>
+    <div class="bg-stone-500/20 w-fit p-3 rounded-md mt-2">
+      <label for="username">
+        Username:
+        <input
+          class="text-black px-2 py-1 rounded"
+          id="username"
+          type="text"
+          placeholder="Username"
+          maxlength="50"
+          [(ngModel)]="username"
+        />
+      </label>
+      <button (click)="onSubmit()">Submit</button>
+    </div>
+    <p class="h-6">{{ username }}</p>
+  `,
+})
+export class TemplateFormComponent {
+  username = '';
+
+  onSubmit() {
+    alert(`Your username is ${this.username}`);
+  }
+}
+
+// TIP: [(ngModel)] is a two-way data binding syntax in Angular. Similar to value={username} onChange={(e) => setUsername(e.target.value)} in React
