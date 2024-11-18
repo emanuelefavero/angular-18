4eefef85e90be8b69fef1ecab31eae53330c0ede
emanuelefavero <emafaux89@gmail.com>
2024-11-18 07:35:36 +0100

    add counter and fetch data component

---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CounterService } from '@/app/services/counter.service';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  template: `
+    <h2 class="mt-4 text-2xl font-bold">Counter</h2>
+    <p [attr.data-testid]="'counter' + counterService.count()">
+      {{ counterService.count() }}
+    </p>
+    <button (click)="counterService.increment()">Increment</button>
+    <button (click)="counterService.decrement()">Decrement</button>
+    <button (click)="counterService.reset()">Reset</button>
+  `,
+})
+export class CounterComponent {
+  constructor(public counterService: CounterService) {}
+}
+
+// TIP: use [attr.ATTRIBUTE] if you want to add an attribute that is not built in like [class]
+// TIP: Remember to use `public` in the constructor to make the service available in the template
---
+++ src/app/components/fetch-data/fetch-data.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { UsersService } from '@/app/services/users.service';
+
+@Component({
+  selector: 'app-fetch-data',
+  standalone: true,
+  template: `
+    <h2 class="mt-4 text-2xl font-bold">Fetch Data</h2>
+    @defer {
+    <ul>
+      @for (user of usersService.users(); track user.id) {
+      <li>
+        <span class="font-bold">
+          {{ user.name }}
+        </span>
+        <span class="ml-2 font-light">
+          {{ user.email }}
+        </span>
+      </li>
+      }
+    </ul>
+    } @placeholder {
+    <p>No users found</p>
+    } @loading (minimum 0.4s) {
+    <p>Loading...</p>
+    }
+  `,
+})
+export class FetchDataComponent {
+  constructor(public usersService: UsersService) {}
+}
