5e3985144c8235412a04976a778644a9e0903378
emanuelefavero <emafaux89@gmail.com>
2024-11-18 07:41:03 +0100

    add reactive-form component

---
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <h2 class="mt-4">Reactive Form:</h2>
+    <form
+      [formGroup]="emailForm"
+      (ngSubmit)="handleSubmit()"
+      class="bg-stone-500/20 w-fit p-3 rounded-md mt-2"
+    >
+      <label for="email">
+        Email:
+        <input
+          class="text-black px-2 py-1 rounded"
+          id="email"
+          type="email"
+          placeholder="Email"
+          maxlength="50"
+          formControlName="email"
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    <p class="h-6">{{ emailForm.value.email }}</p>
+  `,
+})
+export class ReactiveFormComponent {
+  emailForm = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  handleSubmit() {
+    alert(`Your email is ${this.emailForm.value.email}`);
+  }
+}
